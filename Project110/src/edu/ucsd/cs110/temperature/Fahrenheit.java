8e5eee41d78ce52bc39593842b42e28ed3958aa2
Gokul Suresh <gokul.suresh24@gmail.com>
2017-02-15 12:50:44 -0800

    message

---
+++ Project110/src/edu/ucsd/cs110/temperature/Fahrenheit.java
@@ -0,0 +1,28 @@
+package edu.ucsd.cs110.temperature;
+public class Fahrenheit extends Temperature
+{
+    public Fahrenheit(float t)
+    {
+        super(t);
+    }
+
+    @Override
+    public Temperature toCelsius() {
+        float value = ((this.getValue() - 32) * 5) / 9;
+
+        return new Celsius(value);
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        return new Fahrenheit(this.getValue());
+    }
+
+    public String toString()
+    {
+        String f = "";
+        f += this.getValue();
+        f += " F";
+        return f;
+    }
+}
